fa640578f06a618afdd103fc52b0320b129463be
wastilla <wastilla@email.unc.edu>
2023-07-14 12:01:20 -0400

    Reformat start button sizing

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -5,10 +5,10 @@ export const BottomBar = () => {
 
     return(
         <div className="flex flex-row h-fit bg-[url('/src/assets/bottom-bar.png')] bg-repeat-x sm:bg-contain bg-cover items-center">
-            <div className="basis-11/12">
-                <img className="scale-125" src='/src/assets/start-button.png' alt="folder-image"/>
+            <div className="basis-11/12 -mt-1.5 -mb-2">
+                <img className="h-12" src='/src/assets/start-button.png' alt="folder-image"/>
             </div>
-            <Datetime className="basis-1/12 pr-20"/>
+            <Datetime className="basis-1/12 pr-20 align-middle"/>
         </div>
     )
 }
