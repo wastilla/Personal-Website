cd4a1479fd360de19c94cf79b36daed6fe610785
wastilla <wastilla@email.unc.edu>
2023-07-19 19:57:39 -0400

    Added addtional configurations to successfully preview on local

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx', // Specify the entry point as main.jsx
-      },
-    },
-  }
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: './src/main.jsx', 
+  //     },
+  //   },
+  // }
 })
