90cafbdfdc59950b98a0446bc809eb3806a0005a
wastilla <wastilla@email.unc.edu>
2023-07-19 19:29:02 -0400

    Add additional configurations for build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Personal-Website",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx', // Specify the entry point as main.jsx
+      },
+    },
+  }
 })
