c641c720af7f2c75c9715c85cd18bbdd4910c2b6
wastilla <wastilla@email.unc.edu>
2023-07-19 20:24:07 -0400

    Hardcode repo name in index.html

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Website/',
+  base: '/Personal-Website/',
   // build: {
   //   outDir: 'dist',
   //   base: '/Portfolio-Website/',
