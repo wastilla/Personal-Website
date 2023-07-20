81fd21372334ba3b7dd925ea43766dbad8c03305
wastilla <wastilla@email.unc.edu>
2023-07-19 20:19:01 -0400

    re-add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    base: '/Portfolio-Website/',
-    minify: true,
-  },
+  base: '/Portfolio-Website/',
+  // build: {
+  //   outDir: 'dist',
+  //   base: '/Portfolio-Website/',
+  //   minify: true,
+  // },
 })
