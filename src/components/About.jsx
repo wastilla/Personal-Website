58bc344e568a10b525657edc60ab8ec5eb28e5a3
wastilla <wastilla@email.unc.edu>
2023-07-21 17:38:59 -0400

    Change orientation of about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,23 +6,23 @@ const About = (props) => {
   return (
     <div>
       <div className="flex flex-row px-1 space-x-0.5">
-          <button className = "bg-white border border-gray-400 rounded-t-lg w-fit text-black px-2">About</button>
+          <button className = "bg-white border border-gray-400 rounded-t-lg w-fit text-black px-2">General</button>
       </div>
       <div className="p-3 bg-white drop-shadow-2xl border border-gray-400">
         <div className="flex items-center">
-          <div className="grid grid-cols-2 gap-3 text-black">
+          <div className="flex flex-col text-black w-full">
             <div className='self-center justify-center align-middle'>
               <img className="scale-25" src={websiteLogo} alt="folder-image"/>
             </div>
-            <div className="self-center">
-              <div className="text-2xl">William Astilla
-                <div className="pl-5">
+            <div className="md:text-4xl self-center justify-center align-middle">
+              <div className="">
+                <div className="">
                   iOS Developer
                 </div>
                 <div className="pl-5">
                   Frontend Developer
                 </div>
-                <div className="pl-5">
+                <div className="pl-10">
                   Student at UNC Chapel Hill 
                 </div>
               </div>
