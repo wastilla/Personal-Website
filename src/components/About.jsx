87be1039fe8d4d3bef841ee71fe4ca14401ed4ff
wastilla <wastilla@email.unc.edu>
2023-08-04 15:05:11 -0400

    re-add right border for general tab

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = (props) => {
   return (
     <div>
       <div className="flex flex-row px-1">
-          <button className = "shadow-inner bg-white border border-gray-400 rounded-t-lg w-fit text-black px-2 border-r-0 border-b-0" onClick={() => handleButtonClick(<General/>)}>General</button>
+          <button className = "shadow-inner bg-white border border-gray-400 rounded-t-lg w-fit text-black px-2 border-r-1 border-b-0" onClick={() => handleButtonClick(<General/>)}>General</button>
           {/* <button className = "bg-white border border-gray-400 rounded-t-lg w-fit text-black px-2 border-b-0 " onClick={() => handleButtonClick(<div></div>)}>Interests</button> */}
       </div>
       <div className="p-3 bg-white drop-shadow-2xl border border-gray-400">
