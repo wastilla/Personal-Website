24f41a55f783789ca50a6e260153f949cad87323
wastilla <wastilla@email.unc.edu>
2023-08-03 20:41:19 -0400

    Edit bottom bar css to stop bar background from shifting on page resize

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -5,11 +5,11 @@ import { startButton } from '../assets';
 export const BottomBar = () => {
 
     return(
-        <div className="flex flex-row h-fit bg-[url('/src/assets/bottom-bar.png')] bg-repeat-x sm:bg-contain bg-cover items-center">
+        <div className="flex flex-row h-fit bg-[url('/src/assets/bottom-bar.png')] bg-repeat-x bg-contain items-center">
             <div className="basis-11/12 -mt-1.5 -mb-2">
                 <img className="h-12" src={startButton} alt="folder-image"/>
             </div>
-            <div className="pl-3 pr-3 flex basis-1/12 bg-[url('/src/assets/tb-2.png')] bg-repeat-x bg-cover shadow sm:bg-contain h-8 -mt-2 -mb-2 rounded-l-lg">
+            <div className="pl-3 pr-3 flex basis-1/12 bg-[url('/src/assets/tb-2.png')] bg-repeat-x bg-cover shadow  h-8 -mt-2 -mb-2 rounded-l-lg">
                 <Datetime className="  align-middle self-center justify-center"/>
             </div>
             
